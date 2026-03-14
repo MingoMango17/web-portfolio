67532f7d64e781fa7045aeb723f76e1367e03f4c
jinx <junry.mingo17@gmail.com>
2026-03-14 14:02:19 +0800

    feat: add experience timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Nav from "@/components/Nav";
 import Main from "@/components/Main";
 import About from "@/components/About";
+import Experience from "@/components/Experience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -11,8 +13,10 @@ export default function Home() {
       <Nav />
       <Main />
       <About />
+      <Experience />
       <Skills />
       <Projects />
+      <Contact />
       <Footer />
     </main>
   );
